a5b790cf21bb79d3fececbba24c690253691870c
Phenriqx <pedroxlbd@gmail.com>
2025-11-04 19:46:08 -0300

    main; imprimirFormula e adicionarClausula

--- formula.c
+++ formula.c
@@ -4,6 +4,8 @@
 
 #include "formula.h"
 
+char intToChar(int v);
+
 struct clausula {
     int val[3];
     bool valorLogico[3];
@@ -39,4 +41,27 @@ void destroiFormula(Formula** f) {
 
     free(*f);
     *f = NULL;
+}
+
+void imprimeFormula(Formula *f) {
+    for (int i = 0; i < f->m; i++) {
+        printf("( ");
+        for (int j = 0; j < f->n; j++) {
+            if (f->clausulas[i].val[j] > 0)
+                printf("%c ", intToChar(f->clausulas[i].val[j]));
+            else 
+                printf("~%c ", intToChar(f->clausulas[i].val[j] * -1));
+            if (j < f->n - 1)
+                printf(" V ");
+        }
+        printf(" )");
+        if (i < f->m - 1)
+            printf(" ∧ ");
+    }
+
+    printf("\n");
+}
+
+char intToChar(int v) {
+    return v + 'a' - 1;
 }
